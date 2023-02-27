9b32a83b7b2643222f0c16b22c47301ee6be1a5c
Chrys11 <cschris109@gmail.com>
2023-02-27 04:55:44 -0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "HGiOG",
+    subtitle: "Music 'till the Heart",
+    description:
+      "A music school management system to handle music lessons and indtrument dynamics",
+    image: "./project-3.gif",
+    link: "http://jobidy.000webhostapp.com",
+  },
+  {
+    title: "Ebola Tracking App",
+    subtitle: "Infectious but Interesting",
+    description:
+      "Dangerous times, call for smart measures. This webapp is intended to handle the spread of the Ebola Infection in Uganda",
+    image: "./project-4.gif",
+    link: "HERE IS LINK TO PROJECT",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "This gentleman is interesting in the way he handles projects. He has the passion backed with the skills. I believe that his potential is unparralled.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Tugume Daniel",
+    company: "Dant Labs",
+  },
+  {
+    quote:
+      "I have few words to describe his work. Good, better, best and Perfect. He learns fast and can handle all sorts of projects.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Muwonge Ezra",
+    company: "Oasis Corps.",
+  },
+];
+
+export const skills = [
+  "ReactJS",
+  "Django",
+  "NodeJS",
+  "Google Charts",
+  "Desktop Development",
+  "Laravel"
+];
